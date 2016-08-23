aaa8118dddd4f428538771d5e326739d2a908c45
DullSpy <wujingang1118@outlook.com>
2016-08-23 21:20:41 +0800

    add mvc s1

---
+++ DotNet/AspDotNet MVC/mvc_s1/mvc_s1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(mvc_s1.Startup))]
+namespace mvc_s1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
